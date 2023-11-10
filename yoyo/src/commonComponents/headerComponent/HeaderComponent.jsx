70b1c7b38442c959ae7cd02c42094b36342a74a8
kiranbabup <kiran12146@gmail.com>
2023-11-10 15:55:08 +0530

    initial setup and routes done

---
+++ yoyo/src/commonComponents/headerComponent/HeaderComponent.jsx
@@ -0,0 +1,14 @@
+import './headerComponent.css';
+import { useNavigate } from "react-router-dom";
+
+const HeaderComponent = () => {
+    const navigate = useNavigate();
+    
+  return (
+    <div className="HeaderComponent">
+      header
+    </div>
+  );
+}
+
+export default HeaderComponent;
